d3ef92ce5624bec587aed24ed7f17567cd79fdcf
ChandraShekhar <csyadav0513@gmail.com>
2025-06-29 10:12:36 +0530

    feat: add menu types interfaces for category, menu item, page info, and view mode

---
+++ src/components/menuComp/menu-types.ts
@@ -0,0 +1,27 @@
+// app/(dashboard)/menu/components/menu-types.ts
+
+export interface Category {
+  id: number;
+  name: string;
+  cafeId: number;
+}
+
+export interface MenuItem {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  food_image_url: string;
+  is_available: boolean;
+  isSpecial: boolean;
+  tags: string[];
+  categoryId: number;
+  cafeId: number;
+}
+
+export interface PageInfo {
+  currentPage: number;
+  totalPages: number;
+}
+
+export type ViewMode = "grid" | "list";
