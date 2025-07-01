9e28c30dfce0121417b3fce5a39c05dd07ef0466
ChandraShekhar <csyadav0513@gmail.com>
2025-07-02 01:26:44 +0530

    feat: integrate cafe logo and name in AppSidebar from backend API

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -27,6 +27,9 @@ import {
 } from "@/components/ui/sidebar";
 import { Sticker, Utensils } from "lucide-react";
 import Link from "next/link";
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+import Image from "next/image";
 
 const data = {
   user: {
@@ -92,7 +95,33 @@ const data = {
   ],
 };
 
+type CafeData = {
+  name: string;
+  logoUrl: string;
+};
+
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [cafe, setCafe] = React.useState<CafeData | null>(null);
+  const { user, isLoaded } = useUser();
+
+  React.useEffect(() => {
+    if (!isLoaded || !user?.id) return;
+
+    const fetchCafe = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/cafe/name/${user.id}`
+        );
+        setCafe(res.data.cafe);
+      } catch (error) {
+        console.error("Failed to fetch cafe:", error);
+      }
+    };
+
+    fetchCafe();
+  }, [isLoaded, user?.id]);
+
+
   return (
     <Sidebar collapsible="offcanvas" {...props}>
       <SidebarHeader>
@@ -102,21 +131,35 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               asChild
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
-              <Link href="/dashboard">
-                <IconCoffee className="!size-5" />
-                <span className="text-base font-semibold">Chai Cafe</span>
+              <Link href="/dashboard" className="flex items-center gap-2 hover:bg-transparent">
+                {cafe?.logoUrl ? (
+                  <Image
+                    src={cafe.logoUrl}
+                    alt="Cafe Logo"
+                    className="size-6 rounded-full "
+                    width={40}
+                    height={40}
+                  />
+                ) : (
+                  <IconCoffee className="!size-5" />
+                )}
+                <span className="text-xl font-semibold tracking-tight capitalize">
+                  {cafe?.name}
+                </span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
+
       <SidebarContent>
         <NavMain items={data.navMain as any} />
         <NavDocuments items={data.documents} />
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
+
       <SidebarFooter>
-        <NavUser  />
+        <NavUser />
       </SidebarFooter>
     </Sidebar>
   );
