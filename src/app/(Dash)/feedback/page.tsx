a57a7e2661a408e145e7242334033ed715eaedb9
ChandraShekhar <csyadav0513@gmail.com>
2025-07-04 13:36:29 +0530

    feat: add ComingSoon component for user feedback page with image and description

--- src/app/(Dash)/feedback/page.tsx
+++ src/app/(Dash)/feedback/page.tsx
@@ -1,7 +1,13 @@
+import { ComingSoon } from "@/components/ComingSoon"
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <ComingSoon
+      description="🚀 Collect valuable user feedback and improve your platform!"
+      pageName="Feedback Page 📝"
+      />
+    </div>
   )
 }
 export default page
