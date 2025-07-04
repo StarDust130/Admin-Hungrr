6d1b20dcdd6c64f73cd1d76fb34d879bd9aa7281
ChandraShekhar <csyadav0513@gmail.com>
2025-07-04 13:42:14 +0530

    feat: implement ComingSoon component in analytics page for enhanced user experience

--- src/app/(Dash)/analytics/page.tsx
+++ src/app/(Dash)/analytics/page.tsx
@@ -1,4 +1,12 @@
+import { ComingSoon } from "@/components/ComingSoon";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <ComingSoon
+      pageName="📊 Cafe Analytics"
+      description="Track how your cafe is doing with real-time insights, stats, and more. We're cooking up some powerful data just for you! 🍽️📈"
+    />
+  );
 };
+
 export default page;
